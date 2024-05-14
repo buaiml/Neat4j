08a934f15f4fb667b5ffd412e4f64a91662fc60c
Collin Barber <collinjbarber@gmail.com>
2024-05-13 20:59:33 -0400

    remove compute shader experiments

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-
 plugins {
     kotlin("jvm") version "1.9.23"
 }
@@ -12,24 +10,6 @@ repositories {
 }
 
 dependencies {
-    val lwjglVersion = "3.3.3"
-    val lwjglNatives = "natives-linux"
-
-    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
-
-    implementation("org.lwjgl", "lwjgl")
-    implementation("org.lwjgl", "lwjgl-assimp")
-    implementation("org.lwjgl", "lwjgl-glfw")
-    implementation("org.lwjgl", "lwjgl-openal")
-    implementation("org.lwjgl", "lwjgl-opengl")
-    implementation("org.lwjgl", "lwjgl-stb")
-    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
-
     implementation("org.joml:joml:1.10.5")
     implementation("de.m3y.kformat:kformat:0.11")
 
--- src/main/kotlin/readResource.kt
+++
@@ -1,6 +0,0 @@
-fun test(javaClass: Class<*>) {
-    val pathToShader = "compute/doubler.glsl"
-    val resourceStream = javaClass.getResourceAsStream(pathToShader)!!
-    val content = resourceStream.bufferedReader().use { it.readText() }
-    println(content)
-}
